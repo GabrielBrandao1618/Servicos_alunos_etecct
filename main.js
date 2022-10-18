d51f9ec6513940d9e7d3fee3b931682561cbc58d
Gabriel C. Brandão <biel.brandao2004@gmail.com>
2022-10-18 08:06:56 -0300

    Atualizada forma de renderização dos itens na tabela

--- main.js
+++ main.js
@@ -1,28 +1,11 @@
-import {getStudentsData} from './api.js'
-import {Table} from './table.js'
-
-const table = new Table(document.querySelector('.table'))
-
-function handleOpenDetails(student){
-    sessionStorage.setItem('student', JSON.stringify(student))
-    window.location.assign('./detail')
-}
+import {getStudentsData} from './api.js';
+import {Table} from './table.js';
 
 async function main(){
-    const apiData = await getStudentsData()
-    apiData.map(student => {
-        table.addRow(
-            [
-                student.nome,
-                student.nota1,
-                student.nota2,
-                student.nota3,
-                student.media,
-                student.status,
-            ],
-            () => handleOpenDetails(student)
-        )
-    })
+    const apiData = await getStudentsData();
+    const table = new Table(document.querySelector('.table')).setData(apiData);
+
+    table.renderItems(); 
 }
 
-main()
+main();
--- table.js
+++ table.js
@@ -1,3 +1,5 @@
+import {handleOpenDetails} from './handleOpenDetails.js';
+
 export function createCell(content){
     const cell = document.createElement('td')
     cell.textContent = content
@@ -5,20 +7,56 @@ export function createCell(content){
 }
 
 export class Table {
-    tableNode = null
+    tableNode = null;
+    bodyNode = null;
+    data = [];
+    displayData = [];
     constructor(tableNode){
-        this.tableNode = tableNode
+        this.tableNode = tableNode;
+        this.bodyNode = this.tableNode.querySelector('tbody');
+        return this;
     }
 
     addRow(tds, onClick){
-        const row = document.createElement('tr')
-        row.onclick = onClick
-        const cells = tds.map(createCell)
+        const row = document.createElement('tr');
+        row.onclick = onClick;
+        const cells = tds.map(createCell);
         
         cells.map(cell => {
-            row.appendChild(cell)
+            row.appendChild(cell);
         })
 
-        this.tableNode.appendChild(row)
+        this.bodyNode.appendChild(row);
+    }
+    setData(data){
+        this.data = data;
+        this.displayData = data;
+        return this;
+    }
+    renderItems(){
+        this.bodyNode.innerHTML = '';
+
+        this.displayData.map(student => {
+            this.addRow(
+                [
+                    student.nome,
+                    student.nota1,
+                    student.nota2,
+                    student.nota3,
+                    student.media,
+                    student.status,
+                ],
+                () => handleOpenDetails(student)
+            
+            );
+        });
+    }
+    filterByStatus(status){
+        if(status.trim() === ''){
+            this.displayData = this.data;
+            return this;
+        }
+        this.displayData = this.data.filter(student => student.status === status);
+        this.renderItems();
     }
 }
